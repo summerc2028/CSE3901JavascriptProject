8f6e248c751f6ae34d0ff42d989e55640812800b
Derek DiCillo <dddicillo@gmail.com>
2014-02-23 17:18:27 -0500

    Comment searchByDate.js

--- searchByDate.js
+++ searchByDate.js
@@ -11,6 +11,7 @@ function getNearestDate(date) {
          return false;   
     }
 
+    // Get formatted date string
 	var day = date.getDate();
 	day = day < 10 ? "0" + day : day; // force 2 digits
 	var month = date.getMonth() + 1;
@@ -20,9 +21,10 @@ function getNearestDate(date) {
 	document.writeln(fullDate);
 	var fragment = document.getElementById(fullDate);
 
+	// Search for an id matching the date
 	if (typeof(fragment) == 'undefined' || fragment == null)
 	{
- 	    var nextDate = new Date(date.getTime() + (24 * 60 * 60 * 1000));
+ 	    var nextDate = new Date(date.getTime() + (24 * 60 * 60 * 1000)); // increment date
     	return getNearestDate(nextDate);
 	} else {
 		return fullDate;
@@ -34,10 +36,12 @@ function noDate() {
 	alert("There is no class scheduled within the next 7 days!");
 }
 
+// Get nearsest date and the hyperlink element
 var date = new Date();
 var nearestDate = getNearestDate(date);
 var link = document.getElementById("search-by-date");
 
+// Set the onClick function of the link appropriately
 if (nearestDate) {
 	link.setAttribute("href", "#" + nearestDate);
 } else {
