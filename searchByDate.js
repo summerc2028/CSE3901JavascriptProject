4259157cd0c2ad45b55755ff94eef3bb46572c8b
Derek DiCillo <dddicillo@gmail.com>
2014-02-23 16:32:32 -0500

    Fix bugs in search by date javascript

--- searchByDate.js
+++ searchByDate.js
@@ -8,13 +8,13 @@ function getNearestDate(date) {
     if (getNearestDate.counter > 7) {
          return false;   
     }
-    document.write(getNearestDate.counter + "\t");
+
 	var day = date.getDate();
 	var month = date.getMonth() + 1;
 	var year = date.getFullYear();
-	var fullDate = "#" + day + "-" + month + "-" + year;
-	document.writeln(fullDate + "<br>");
+	var fullDate = day + "-" + month + "-" + year;
 	var fragment = document.getElementById(fullDate);
+
 	if (typeof(fragment) == 'undefined' || fragment == null)
 	{
  	    var nextDate = new Date(date.getTime() + (24 * 60 * 60 * 1000));
@@ -31,12 +31,9 @@ function noDate() {
 var date = new Date();
 var nearestDate = getNearestDate(date);
 var link = document.getElementById("search-by-date");
-document.write("Enter if statement")
+
 if (nearestDate) {
-	document.write("nearestDateFound")
-	link.setAttribute("href", nearestDate);
+	link.setAttribute("href", "#" + nearestDate);
 } else {
-	document.write("nearestDateNotFound")
 	link.addEventListener("click", noDate);
 }
-document.write("DONE")
