99fb7f03e016c3416cb596bfa5ee93b16e982e70
Derek DiCillo <dddicillo@gmail.com>
2014-02-27 18:27:11 -0500

    Refactor some event handlers

--- calculator.js
+++ calculator.js
@@ -1,5 +1,29 @@
+	var prevOp;
 	var mem = 0;
 
+	function initializeCells() {
+		display.b0.addEventListener("click", function(){addDigit(0)}, false);
+		display.b1.addEventListener("click", function(){addDigit(1)}, false);
+		display.b2.addEventListener("click", function(){addDigit(2)}, false);
+		display.b3.addEventListener("click", function(){addDigit(3)}, false);
+		display.b4.addEventListener("click", function(){addDigit(4)}, false);
+		display.b5.addEventListener("click", function(){addDigit(5)}, false);
+		display.b6.addEventListener("click", function(){addDigit(6)}, false);
+		display.b7.addEventListener("click", function(){addDigit(7)}, false);
+		display.b8.addEventListener("click", function(){addDigit(8)}, false);
+		display.b9.addEventListener("click", function(){addDigit(9)}, false);
+	}
+
+	function addChar(input, character) {
+		if(input.value == null || input.value == "0"){
+			input.value = character
+		}
+		else{
+			input.value += character
+		}
+	}
+
+	// Memory Functions
 	function memadd (x) {
 		mem = parseInt (x.form.display.value) + parseInt (mem);
 	}
@@ -16,32 +40,15 @@
 		mem = x.form.display.value;
 	}
 
-	function addChar(input, character) {
-		if(input.value == null || input.value == "0"){
-			input.value = character
-		}
-		else{
-			input.value += character
-		}
-	}
-
-	function delChar(input) {
-		input.value = input.value.substring(0, input.value.length - 1)
-	}
-
-	function calculate(form)  {
-		form.display.value = eval(form.display.value)
-	}
-
-	function checkNum(str)  {
-		for (var i = 0; i < str.length; i++) {
-			var y = str.substring(i, i+1);
-			if (y < "0" || y > "9") {
-				if (y != "/" && y != "*" && y != "+" && y !="-" && y != ".") {
-					alert("not a valid input.")
-					return false;
-				}
-			}
-		}
-		return true;
-	}
+	// function checkNum(str)  {
+	// 	for (var i = 0; i < str.length; i++) {
+	// 		var y = str.substring(i, i+1);
+	// 		if (y < "0" || y > "9") {
+	// 			if (y != "/" && y != "*" && y != "+" && y !="-" && y != ".") {
+	// 				alert("not a valid input.")
+	// 				return false;
+	// 			}
+	// 		}
+	// 	}
+	// 	return true;
+	// }
